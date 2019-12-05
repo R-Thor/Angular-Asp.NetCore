23acb2767b80d5ed496444295c9a544c20b518bb
R-Thor <swaybo@yahoo.com>
2019-12-05 03:28:33 -0500

    Playing with Asp.Net Core with Angular

---
+++ WebApp-Angular-02/WebApp-Angular-02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApp_Angular_02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
